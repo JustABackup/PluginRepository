347b42bfb2eb995f55168467b44c752c70022304
trembon <trembon@hotmail.com>
2019-01-30 23:08:50 +0100

    more bootstrap

---
+++ src/JustABackup.PluginRepository/Controllers/DetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustABackup.PluginRepository.Controllers
+{
+    public class DetailsController : Controller
+    {
+        [Route("/details/{package}")]
+        public IActionResult Package(string package, string version = null)
+        {
+            return View();
+        }
+    }
+}
